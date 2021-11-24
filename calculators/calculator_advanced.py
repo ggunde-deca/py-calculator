1214d5e5b5084f6ab65468ec55f9601729a23538
Gaurang Gunde <gaurang.gunde@thinkdeca.com>
2021-11-23 17:38:23 -0700

    Making use of self to prevent prospector complaint

--- calculators/calculator_advanced.py
+++ calculators/calculator_advanced.py
@@ -2,20 +2,30 @@ from interface.calculator_interface import Calculator
 
 
 class AdvancedCalculator(Calculator):
+    def __init__(self) -> None:
+        super().__init__()
+        self.result = 0
+
     def add(self, a, b):
-        return a + b
+        self.result = a + b
+        return self.result
 
     def subtract(self, a, b):
-        return a - b
+        self.result = a - b
+        return self.result
 
     def multiply(self, a, b):
-        return a * b
+        self.result = a * b
+        return self.result
 
     def divide(self, a, b):
-        return a / b
+        self.result = a / b
+        return self.result
 
     def bitwise_and(self, a, b):
-        return a & b
+        self.result = a & b
+        return self.result
 
     def bitwise_or(self, a, b):
-        return a | b
+        self.result = a | b
+        return self.result
