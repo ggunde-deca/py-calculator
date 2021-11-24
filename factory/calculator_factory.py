007ee33753306cb60d788507c402e25ff83e013b
Gaurang Gunde <gaurang.gunde@thinkdeca.com>
2021-11-23 17:31:21 -0700

    Added tests for bitwise and or

--- factory/calculator_factory.py
+++ factory/calculator_factory.py
@@ -1,11 +1,10 @@
-from calculators.calculator_basic import BasicCalculator
 from calculators.calculator_advanced import AdvancedCalculator
+from calculators.calculator_basic import BasicCalculator
 
 
 def calculator_factory(calculator_type):
     if calculator_type == "basic":
         return BasicCalculator()
-    elif calculator_type == "advanced":
+    if calculator_type == "advanced":
         return AdvancedCalculator()
-    else:
-        raise TypeError("The calculator type is unsupported")
+    raise TypeError("The calculator type is unsupported")
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -27,3 +27,17 @@ def test_divide():
 
     divide = calculator_var.divide(5, 6)
     assert (int)(divide) == 0
+
+
+def test_bitwise_and():
+    calculator_var = calculator_factory("advanced")
+
+    anded = calculator_var.bitwise_and(1, 0)
+    assert anded == 0
+
+
+def test_bitwise_or():
+    calculator_var = calculator_factory("advanced")
+
+    ored = calculator_var.bitwise_or(1, 0)
+    assert ored == 1
